3d2f93a5b4bac4432d7d277a3c1fb912c89e73d5
Julian Luís <julian.lfs@gmail.com>
2019-07-17 14:34:17 -0300

    Initial commit

---
+++ src/main/java/br/com/julianlfs/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,14 @@
+package br.com.julianlfs.microservices.limitsservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @GetMapping("/limits")
+    public LimitsConfiguration retrieveLimitsFromConfigurations(){
+        return new LimitsConfiguration(1000, 1);
+    }
+
+}
