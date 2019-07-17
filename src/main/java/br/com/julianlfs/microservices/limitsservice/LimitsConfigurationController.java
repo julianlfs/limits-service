e21d38c4be14cb6c12afeec29891ab15eb91c84a
Julian Luís <julian.lfs@gmail.com>
2019-07-17 14:50:13 -0300

    adicionado confifurações de limite no db.properties

--- src/main/java/br/com/julianlfs/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/br/com/julianlfs/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,14 +1,18 @@
 package br.com.julianlfs.microservices.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration config;
+
     @GetMapping("/limits")
     public LimitsConfiguration retrieveLimitsFromConfigurations(){
-        return new LimitsConfiguration(1000, 1);
+        return new LimitsConfiguration(config.getMaximum(), config.getMinimum());
     }
 
 }
