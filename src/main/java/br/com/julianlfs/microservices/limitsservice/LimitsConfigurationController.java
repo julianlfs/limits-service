1000d6296cf898f71e9d8498966c4a199b90e411
Julian Luís <julian.lfs@gmail.com>
2019-08-03 21:50:46 -0300

    hytrix

--- src/main/java/br/com/julianlfs/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/br/com/julianlfs/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package br.com.julianlfs.microservices.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,14 @@ public class LimitsConfigurationController {
     private Configuration config;
 
     @GetMapping("/limits")
+    @HystrixCommand(fallbackMethod = "fallbackConfiguration")
     public LimitsConfiguration retrieveLimitsFromConfigurations(){
         return new LimitsConfiguration(config.getMaximum(), config.getMinimum());
     }
+
+    public LimitsConfiguration fallbackConfiguration() {
+        return new LimitsConfiguration(999, 9);
+    }
+
 
 }
